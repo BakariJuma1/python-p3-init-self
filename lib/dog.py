a37bdb95ba4f2716e230acf354336c1e90c12bc2
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-05-13 12:01:19 +0300

    using the self

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self,name,breed='Mutt'):
+        self.name=name
+        self.breed=breed
+        print(f"this is my name {self.name}")
+      
+
+fido=Dog("Fido")
